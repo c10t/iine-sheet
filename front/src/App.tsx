907bc11bf2c292866ef7aecf47c6be2e648fc41e
c10t <c10t@fa.ke>
2018-09-02 21:38:32 +0900

    get started with sample grid

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './App.css';
-
+import MainGrid from './components/MainGrid';
 import logo from './logo.svg';
+
 
 class App extends React.Component {
   public render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <MainGrid dummy="hoge" />
       </div>
     );
   }
---
+++ front/src/components/MainGrid.tsx
@@ -0,0 +1,66 @@
+
+import { createStyles } from "@material-ui/core";
+import { withStyles, WithStyles } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+// import {Grid, GridOptions} from "ag-grid/main";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid/dist/styles/ag-grid.css";
+import "ag-grid/dist/styles/ag-theme-balham.css";
+import * as React from "react";
+
+const styles = createStyles({
+  AgThemeBalham: {
+    height: '500px',
+    width: '600px'
+  },
+  paper: {
+    padding: 10,
+  },
+  root: {
+    padding: 10,
+  },
+});
+
+type ClassNames = keyof typeof styles;
+
+export interface IMainGridProps {
+  dummy: string;
+}
+
+class MainGrid extends React.Component<IMainGridProps & WithStyles<ClassNames>,
+    { columnDefs:Array<{}>, rowData:Array<{}> }> {
+  constructor(props: IMainGridProps & WithStyles<ClassNames>) {
+    super(props);
+
+    this.state = {
+      columnDefs: [
+        {headerName: "Make", field: "make"},
+        {headerName: "Model", field: "model"},
+        {headerName: "Price", field: "price"}
+      ],
+      rowData: [
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxter", price: 72000}
+      ]
+    }
+  }
+
+  public render() {
+    const classes = this.props.classes;
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <div className={classes.AgThemeBalham}>
+            <AgGridReact
+              columnDefs={this.state.columnDefs}
+              rowData={this.state.rowData}
+            />
+          </div>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default withStyles<{} & ClassNames, {}>(styles)(MainGrid);
